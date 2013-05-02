90ee61b6277a899ef28a0e951f4a3a2c4d8bfe91
Ben-Alexander Cassell <bcassell@umich.edu>
2013-05-02 14:40:05 -0400

    improvements to ci, plus eq_ci stopping rule

--- sequential/yaml_builder.py
+++ sequential/yaml_builder.py
@@ -1,7 +1,9 @@
-from stopping_rules import StandardErrorEvaluator, EquilibriumCompareEvaluator
-from noise_models import MultimodalNormalNoise
+from stopping_rules import StandardErrorEvaluator, EquilibriumCompareEvaluator, EquilibriumConfidenceEvaluator
+from noise_models import MultimodalNormalNoise, SimulationBasedGame
+from confidence import BootstrapConfidenceInterval
 import RandomGames
-
+import GameIO
+import Reductions
 
 def construct_stopping_rule(input, game):
     options = input.get('options', {})
@@ -9,19 +11,27 @@ def construct_stopping_rule(input, game):
         return StandardErrorEvaluator(input['standard_err_threshold'], game.knownProfiles())
     elif input['type'] == 'equilibrium':
         return EquilibriumCompareEvaluator(input['compare_threshold'], **options)
+    elif input['type'] == 'eq_ci':
+        return EquilibriumConfidenceEvaluator(input['delta'], input['alpha'], BootstrapConfidenceInterval())
 
 def construct_model(stdev, input):
     options = input.get('options', {})
     if input['type'] == 'gaussian':
         return MultimodalNormalNoise(stdev, **options)
+    if input['type'] == 'file':
+        return SimulationBasedGame(Reductions.deviation_preserving_reduction(GameIO.read(input['file']), {'All': 6}))
     
 def construct_game(input):
     options = input.get('options', {})
     if input['type'] == 'local_effect':
         game = RandomGames.local_effect_game(**options)
+        RandomGames.rescale_payoffs(game)
     elif input['type'] == 'congestion':
         game = RandomGames.congestion_game(**options)
+        RandomGames.rescale_payoffs(game)
     elif input['type'] == 'uniform':
         game = RandomGames.uniform_symmetric_game(**options)
-    RandomGames.rescale_payoffs(game)
+        RandomGames.rescale_payoffs(game)
+    elif input['type'] == 'file':
+        game = Reductions.deviation_preserving_reduction(GameIO.read(input['file']), {'All': 6})
     return game
